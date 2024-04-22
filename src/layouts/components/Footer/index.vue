847abbeaaa02bd4690b51b74b9f9a0e83be3da2a
Gale <zhguzibo@163.com>
2024-04-22 19:32:12 +0800

    feat(component): 🚀 first commit

--- src/layouts/components/Footer/index.vue
+++ src/layouts/components/Footer/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer flx-center">
-    <a href="https://github.com/zhguzibo" target="_blank"> 2024 © 掘金视界 By Gale</a>
+    <a href="https://github.com/zhguzibo" target="_blank"> 2024 © 智学管家 By Gale</a>
   </div>
 </template>
 
